fef24cbacedf6b5b816bc6ea944014eb71a4f0c8
Sean Powell <sean@longdivision.co.uk>
2016-01-20 23:35:25 +0000

    Bring in test dependencies. Add simple test for Front Page title.

---
+++ app/src/androidTest/java/com/hexforhn/hex/FrontPageActivityTest.java
@@ -0,0 +1,35 @@
+package com.hexforhn.hex;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.test.ActivityTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+public class FrontPageActivityTest extends ActivityTestCase {
+    @Rule
+    public ActivityTestRule<FrontPageActivity> mActivityRule = new ActivityTestRule<>(
+            FrontPageActivity.class);
+
+    @Test
+    public void testToolbarTitleIsShown() {
+        onView(allOf(
+                withParent(isAssignableFrom(Toolbar.class)),
+                isAssignableFrom(TextView.class))
+            ).check(matches(withText("Front Page")));
+    }
+}
